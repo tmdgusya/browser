064ab522a0dbbae098e81e855cbff6f7e3f98c8a
tmdgusya <dev0jsh@gmail.com>
2025-09-22 23:25:55 +0900

    스크롤 구현

--- src/browser.py
+++ src/browser.py
@@ -5,23 +5,47 @@ from .url import URL
 class Browser:
   def __init__(self) -> None:
     self.WIDTH, self.HEIGHT = 800, 600
+    self.HSTEP, self.VSTEP = 13, 18
+    self.SCROLL_STEP = 100
+    self.display_list = []
+    self.scroll = 0
     self.window = tkinter.Tk()
     self.canvas = tkinter.Canvas(self.window, width=self.WIDTH, height=self.HEIGHT)
     self.canvas.pack()
-    
-  def load(self, url: URL):
-    HSTEP, VSTEP = 13, 18
 
-    text = url.load()
-    cursor_x, cursor_y = HSTEP, VSTEP
+    # key binding
+    self.window.bind("<Down>", self.scrolldown)
+    
+  def scrolldown(self, e):
+    self.scroll += self.SCROLL_STEP
+    self.draw()
+    
+  def layout(self, text):
+    cursor_x, cursor_y = self.HSTEP, self.VSTEP
     for c in text:
-      self.canvas.create_text(cursor_x, cursor_y, text=c)
-      cursor_x += HSTEP
-      if cursor_x >= self.WIDTH - HSTEP:
-        cursor_y += VSTEP
-        cursor_x = HSTEP
+      self.display_list.append((cursor_x, cursor_y, c))
+      cursor_x += self.HSTEP
+      if cursor_x >= self.WIDTH - self.HSTEP:
+        cursor_y += self.VSTEP
+        cursor_x = self.HSTEP
+    
+    return self.display_list
+  
+  def load(self, url: URL):
+    text = url.load()
+    self.layout(text=text)
+    
+  def draw(self):
+    self.canvas.delete("all")
+    for cursor_x, cursor_y, c in self.display_list:
+      if cursor_y > self.scroll + self.HEIGHT: continue
+      if cursor_y + self.VSTEP < self.scroll: continue
+      self.canvas.create_text(cursor_x, cursor_y - self.scroll, text=c)
+
 
 if __name__ == "__main__":
   import sys
-  Browser().load(URL(sys.argv[1]))
+  browser = Browser()
+  browser.load(URL(sys.argv[1]))
+  browser.draw()
   tkinter.mainloop()
